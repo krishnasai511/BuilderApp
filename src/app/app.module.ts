ff33e48b9710ed585616af9be8bbb997c12999ac
unknown <krishnasai511@gmail.com>
2018-04-09 11:43:07 +0530

    Intial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './project/sections/header/header.component';
+import { FooterComponent } from './project/sections/footer/footer.component';
+import { TextareComponent } from './project/sections/textare/textare.component';
+import { ButtonsComponent } from './project/sections/buttons/buttons.component';
+import { HomeComponent } from './project/pages/home/home.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TextareComponent,
+    ButtonsComponent,
+    HomeComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/project/parts/textarea/textarea.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-textare',
+  templateUrl: './textare.component.html',
+  styleUrls: ['./textare.component.css']
+})
+export class TextareComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
